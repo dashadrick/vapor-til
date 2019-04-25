723deb7fc41ed981e7bf4bc90f42330a65bc036a
Dana A. Shadrick <dana@dasiv.com>
2019-04-25 15:32:16 -0500

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,25 @@
+import Vapor
+import FluentSQLite
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: SQLiteModel {}
+//extension Acronym: Model {
+//    // 1
+//    typealias Database = SQLiteDatabase
+//    // 2
+//    typealias ID = Int
+//    // 3
+//    public static var idKey: IDKey = \Acronym.id
+//}
+extension Acronym: Migration {}
+extension Acronym: Content {}
